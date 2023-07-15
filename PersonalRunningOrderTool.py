4ac7cdef0c87ac1fdbca0613ee220d58d6b6268e
Tare <Tare.noN@web.de>
2023-07-15 09:59:26 +0200

    further reordering

--- PersonalRunningOrderTool.py
+++ PersonalRunningOrderTool.py
@@ -17,10 +17,6 @@
 
 import datetime
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import matplotlib.backends.backend_pdf as backend_pdf
-
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
@@ -31,7 +27,7 @@ from classes import Settings
 from classes import Stage
 
 
-from utils import get_timeless_date
+from utils import print_running_order
 from utils import save_file_as_browser
 
 import os
@@ -65,9 +61,9 @@ def parse_lineup(file_path) -> LineUp:
     bands = []
 
     # first thing is to increase the line number, so we can set it to 0, then we "start" at 1
-    lineNumber = 0
+    line_number = 0
     for line in open(file_path, 'r', encoding='utf-8'):
-        lineNumber += 1
+        line_number += 1
         if line.startswith('Band') or line.startswith('#') or line == "\n" or not line:
             # ignore the line, as it is only for description or comment
             continue
@@ -86,7 +82,7 @@ def parse_lineup(file_path) -> LineUp:
             end = data[3]
             stage = data[4].replace('\n', '')
         except:
-            print("Could not parse line ", line, " at line ", lineNumber)
+            print("Could not parse line ", line, " at line ", line_number)
         if not stage_names.__contains__(stage):
             stage_names.append(stage)
 
@@ -95,9 +91,9 @@ def parse_lineup(file_path) -> LineUp:
             date_object = datetime.datetime.strptime(date, '%d.%m.%Y')
         except:
             # TODO: open window showing the user an error occurred and where it occured
-            errMsg = 'Could not parse date on line ' + str(lineNumber) +'\n'
-            errMsg += 'Invalid line: ' + line
-            messagebox.showerror('Parsing error', errMsg)
+            err_msg = 'Could not parse date on line ' + str(line_number) + '\n'
+            err_msg += 'Invalid line: ' + line
+            messagebox.showerror('Parsing error', err_msg)
             return
 
         # parse the time and fit the date to it for a correct datetime object
@@ -125,204 +121,6 @@ def parse_lineup(file_path) -> LineUp:
         days[date].append(band)
 
     return LineUp(stage_names, days, bands)
-
-
-def get_time_clashing_bands(selection):
-    # for every band that is selected, read out the start and end time.
-    # sort by day, then compare within each day each selected band
-    clashing_bands = []
-    global lineup
-    selection_full_info = {}
-
-    for date in lineup.dates:
-        selection_full_info[date] = []  # list[Band]
-
-    for band in selection:
-        band_full_info = lineup.get_full_info(band)
-        band_date = get_timeless_date(band_full_info.start)
-        selection_full_info[band_date].append(band_full_info)
-
-    for date in selection_full_info:
-        bands_on_date = selection_full_info[date]
-        for i in range(len(bands_on_date)):
-            for j in range(i + 1, len(bands_on_date)):
-                band_i = bands_on_date[i]
-                band_j = bands_on_date[j]
-
-                # a band clashes, if either start or end of band_i is between start and end of band_j
-                # (or vice versa)
-                start_i = band_i.start.time().hour + band_i.start.time().minute / 60
-                end_i = band_i.end.time().hour + band_i.end.time().minute / 60
-                start_j = band_j.start.time().hour + band_j.start.time().minute / 60
-                end_j = band_j.end.time().hour + band_j.end.time().minute / 60
-
-                # take care of a time wrap around at 0:00 AM! consider everything <4 as playing later, really
-                # this is not a clean approach (due to hardcoded 4), but should work in most cases
-                if start_i < 4:
-                    start_i += 24
-                if end_i < 4:
-                    end_i += 24
-                if start_j < 4:
-                    start_j += 24
-                if end_j < 4:
-                    end_j += 24
-
-                if (
-                    ((start_i > start_j and start_i < end_j) or (end_i > start_j and end_i < end_j)) or\
-                    ((start_j > start_i and start_j < end_i) or (end_j > start_i and end_j < end_i))
-                    ):
-                    # there is a clash between these bands. add both to the clashing list
-                    if not clashing_bands.__contains__(band_i.name):
-                        clashing_bands.append(band_i.name)
-                    if not clashing_bands.__contains__(band_j.name):
-                        clashing_bands.append(band_j.name)
-
-    return clashing_bands
-
-
-def print_running_order(bands_dict=None):
-    global lineup
-    global settings
-    global stages
-    # TODO: make this alternating for a stage
-    colors = ['lightgray', 'darkgray']
-
-    # get the output path first. all images can be stored accordingly as individual files
-    save_path = save_file_as_browser()
-    if save_file_as_browser == "":
-        # probably the user canceled. therefore, just return here
-        return
-
-    save_dir = os.path.dirname(save_path)
-    file_name = os.path.basename(save_path)
-
-    # read out the selected bands
-    selection = []
-    if bands_dict is not None:
-        for band in bands_dict:
-            if bands_dict[band].get() == 1:
-                selection.append(band)
-
-    # get the selected bands with time clashes
-    clashing_bands = get_time_clashing_bands(selection)
-
-    stage_names = lineup.stages
-    for stage in stages:
-        if not stage.is_enabled():
-            stage_names.remove(stage.name)
-    print(stage_names)
-    figures = []
-
-    # loop all days, each day gets its own image
-    for day in lineup.dates:
-        # create the basic plot figure
-        fig = plt.figure(figsize=(11.69, 8.27))
-        # disable the plot's axes, they will be added in subplots
-        plt.axis('off')
-
-        # TODO: maybe try to hardcode time stamps to get the wrapping at 24/0 o'clock right
-        hours = ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00",
-                 "19:00", "20:00", "21:00", "22:00", "23:00", "0:00", "1:00", "2:00", "3:00", "4:00"]
-
-        # for readability of the resulting plot, offset the x position by 0.5
-        x_offset_axis = 0.5
-
-        # set axes (for bottom left first, then mirror for upper right)
-        axis_bl = fig.add_subplot(111)
-        axis_bl.yaxis.grid()
-        axis_bl.set_xlim(x_offset_axis, len(stage_names) + x_offset_axis)
-        # it will be read downwards, therefore invert the time labels on the axis
-        axis_bl.set_ylim(27.3, 10.9)
-        axis_bl.set_xticks(range(1, len(stage_names) + 1))
-        axis_bl.set_xticklabels(stage_names)
-        axis_bl.set_ylabel('Time')
-        # axis_bl.set_yticks(range(1, len(hours) + 1))
-        # axis_bl.set_yticklabels(hours)
-
-        axis_ur = axis_bl.twiny().twinx()
-        axis_ur.set_xlim(axis_bl.get_xlim())
-        axis_ur.set_ylim(axis_bl.get_ylim())
-        axis_ur.set_xticks(axis_bl.get_xticks())
-        axis_ur.set_xticklabels(stage_names)
-        axis_ur.set_ylabel('Time')
-        # axis_ur.set_yticks(axis_bl.get_yticks())
-        # axis_ur.set_yticklabels(axis_bl.get_yticklabels())
-
-        # add all the band plots
-        for band in lineup.dates[day]:
-            stage = band.stage
-            if stage not in stage_names:
-                continue
-            start = band.start.time().hour + band.start.time().minute / 60
-            # assume that no band will play after 4!
-            # this is a bit hacky, but we need some time wrap around at 23:59->0:00
-            if start < 4:
-                start += 24
-            end = band.end.time().hour + band.end.time().minute / 60
-            if end < 4:
-                end += 24
-
-            # plot the band onto the correct stage
-            col = 'lightgray'
-            if selection.__contains__(band.name):
-                if clashing_bands.__contains__(band.name):
-                    col = 'red'
-                else:
-                    col = 'green'
-            # the rectangle_width is "normalized" to number of stages. i.e. 1 is exactly one stage width, scaling
-            # with the number of stages. with 1, there would be no space between tow adjoining stages
-            rectangle_width = 0.95
-            # take into consideration the x_offset used for the x-axis
-            x_offset = x_offset_axis + (1 - rectangle_width) * 0.5
-            band_rectangle = patches.Rectangle([stage_names.index(stage) + x_offset, start], rectangle_width, end - start,
-                                               color=col,  # colors[int(band.start.time().hour % len(colors))],
-                                               linewidth=0.5)
-            axis_bl.add_patch(band_rectangle)
-
-            # print the actual start time to make it legible
-            y_margin = 0.05
-            time_font_size = 7
-            plt.text(stage_names.index(stage) + x_offset, start + y_margin,
-                     '{0}:{1:0>2}'.format(band.start.time().hour, band.start.time().minute),
-                     va='top', fontsize=time_font_size)
-            # also the end time on the opposite corner (thus preventing it from writing over the start time)
-            # first, just plot the time to get the size of it (and plot it somewhere where it can't be seen)
-            t = plt.text(-10, -10, '{0}:{1:0<2}'.format(band.end.time().hour, band.end.time().minute),
-                          va='top', fontsize=time_font_size)
-            bb = t.get_window_extent(renderer=fig.canvas.get_renderer()).transformed(
-                axis_bl.transData.inverted())
-
-            # now print it into the plot at the correct position
-            # that is: x = end_of_rectangle.x-text_width + x_offset
-            # (where the x_offset is the one used for the x-axis)
-            # and: y = end_of_rectangle.y-text_height
-            x_coord = stage_names.index(stage) + rectangle_width - bb.width + x_offset
-            # for whatever reason, the bbox y value is negative, therefore add it here to avoid a double negative
-            y_coord = end + bb.height
-
-            plt.text(x_coord, y_coord,
-                     '{0}:{1:0<2}'.format(band.end.time().hour, band.end.time().minute),
-                     va='top', fontsize=time_font_size)
-
-            # print the name of the band
-            plt.text(stage_names.index(stage)+1, (start + end) * 0.5, band.name, ha='center', va='center',
-                     fontsize=9)
-
-        day_str = day.strftime("%d.%m.%Y")
-        plt.title(day_str, y=1.07)
-        if settings.save_as_image:
-            file_path = os.path.join(save_dir, '{0}-{1}.png'.format(file_name, day_str))
-            plt.savefig(file_path, dpi=settings.dpi)
-            print('saving {0}'.format(file_path))
-        figures.append(fig)
-
-    # print it all as one pdf
-    if settings.save_as_pdf:
-        pdf = backend_pdf.PdfPages(save_path + '.pdf')
-        for fig in figures:
-            pdf.savefig(fig)
-
-        pdf.close()
 
 
 def browse_files(filetypes=(("All files", "*.*"),), entry_box=None):
@@ -407,6 +205,8 @@ def open_band_selection_window():
     selection_window.wm_attributes('-topmost', 1)
 
     global lineup
+    global settings
+    global stages
 
     # we will want one section with all the bands to choose from, and one with buttons to click
     # to keep them separated and thus not screwing up the layout, put them into individual frames
@@ -471,7 +271,7 @@ def open_band_selection_window():
     clear_button.grid(row=0, column=2)
 
     save_order_button = Button(master=control_frame, text="Save Personal Running Order",
-                               command=lambda: print_running_order(bands_dict))
+                               command=lambda: print_running_order(lineup, settings, stages, bands_dict))
     save_order_button.grid(row=0, column=3)
 
 
@@ -537,9 +337,12 @@ def setup_gui():
     filebrowser_button.grid(row=1, column=2)
 
     # add a button to parse and print the pdf
+    global lineup
+    global settings
+    global stages
     create_running_order_button = Button(master=window, text="Create Complete Running Order",
                                          state=DISABLED,
-                                         command=lambda: print_running_order())
+                                         command=lambda: print_running_order(lineup, settings, stages))
     create_running_order_button.grid(row=2, column=0, columnspan=3)
 
     create_personal_running_order_button = Button(master=window, text="Create Personal Running Order",
